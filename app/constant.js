72884ffe327080ccdfaff226d526e7a347aaf7dd
zhouronghua <zhouronghua>
2017-03-11 09:08:10 +0800

    1.初始化

---
+++ app/constant.js
@@ -0,0 +1,7 @@
+exports = {
+    MY_HOST: 'http://www.mr18.me/',
+    REQUEST_URL: 'http://www.cnblogs.com',
+    SITE_SCRIPT: '<script>var _hmt = _hmt || [];(function() {var hm = document.createElement("script");hm.src = "https://hm.baidu.com/hm.js?3e2feca85a5a456098df289f21a36c09";var s = document.getElementsByTagName("script")[0];s.parentNode.insertBefore(hm, s);})();</script>',
+    ERROR_PAGE: '<!DOCTYPE html><html><head></head><meta http-equiv="Content-Type" content="text/html; charset=utf-8" /><title>Mr18 404 Page</title><body><h1>Mr18 404 Page</h1></body>'
+
+}
--- app/index.js
+++ app/index.js
@@ -3,44 +3,62 @@
  */
 
 var Download = require('../module/download.js');
-//var Crypto = require('crypto');
-//var Md5 = Crypto.createHash('md5',32);
-var myHost = 'http://www.mr18.me/';
-var requestUrl = 'http://www.cnblogs.com';
+var CONSTANT = render('./constant.js');
+
 var getRequestUrl = function (url) {
     url = (url || '').replace(/^\/*/, '');
-    return url ? 'http://' + url : requestUrl;
+    return url ? 'http://' + url : CONSTANT.REQUEST_URL;
 }
 var HostRe = /^(http:|https:)*(\/\/)*([^\/]+)/;
-var aRe = /<a[^>]*>.+<\/a>/ig;
 var hrefRe = /(<a[^>]+href=\")([^\"]+)([^>]*)/ig;
+var headRe = /(<head.*)(<\/head>)/;
 
+var errorPage = function (req, res) {
+    var html = CONSTANT.ERROR_PAGE.replace(headRe, function ($0, $1, $2) {
+        return $1 + CONSTANT.SITE_SCRIPT + $2;
+    })
+    res.status(404);
+    res.send(html);
+}
+var renderPage = function (req, res, html) {
+    html = CONSTANT.ERROR_PAGE.replace(headRe, function ($0, $1, $2) {
+        return $1 + CONSTANT.SITE_SCRIPT + $2;
+    })
+    res.status(200);
+    res.send(html);
+}
+
+var replaceHref = function (html) {
+    html = html || '';
+    html = html.replace(/[\n\t\f\r]*/g, '').replace(hrefRe, function ($0, $1, $2, $3) {
+        var urlHost = ( $2.match(HostRe) || [])[3];
+        var newHref = $2.replace(HostRe, '').replace(/^\/+/, '');
+        if (!/^(#|javascript:)[^/]*/.test(newHref)) {
+            newHref = CONSTANT.MY_HOST + (urlHost ? urlHost : mainHost) + '/' + newHref
+        } else {
+            newHref = $2;
+        }
+        return $1 + newHref + $3
+    });
+    return html;
+}
 var render = function (req, res) {
     var url = getRequestUrl(req.url);
     var hostArr = url.match(HostRe) || [];
     var mainHost = hostArr[3];
-
     //有host
     if (mainHost) {
         Download(url, function (html) {
-            html = html || '';
-            html = html.replace(/[\n\t\f\r]*/g, '')
-            html = html.replace(hrefRe, function ($0, $1, $2, $3) {
-                var urlHost = ( $2.match(HostRe) || [])[3];
-                var newHref = $2.replace(HostRe, '').replace(/^\/+/, '');
-                if (!/^(#|javascript:)[^/]*/.test(newHref)) {
-                    newHref = myHost + (urlHost ? urlHost : mainHost) + '/' + newHref
-                }else{
-                    newHref = $2;
-                }
-                return $1 + newHref + $3
-            });
-            res.status(200);
-            res.send(html);
+            html = replaceHref(html);
+            if (!html) {
+                errorPage(req, res);
+            } else {
+                renderPage(req, res, html);
+            }
+
         })
     } else {
-        res.status(404);
-        res.send('<!DOCTYPE html><html><head></head><body><h1>404 Page</h1></body>');
+        errorPage(req, res);
     }
 
 }
